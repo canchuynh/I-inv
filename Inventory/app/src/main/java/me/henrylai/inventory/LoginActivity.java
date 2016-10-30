8bb08ba7a0ccc94ea5811ef9ecbc72da264cfd03
Henry Lai <bzlai@aol.com>
2016-10-30 00:56:03 -0700

    [UC1] Added activity and layout for login

---
+++ Inventory/app/src/main/java/me/henrylai/inventory/LoginActivity.java
@@ -0,0 +1,29 @@
+package me.henrylai.inventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // UI Components Declared for this Activity
+    public EditText mEmailField;
+    public EditText mPasswordField;
+    public Button mRegisterButton;
+    public Button mLoginButton;
+    public Button mGoogleLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Connecting the UI components
+        mEmailField = (EditText) findViewById(R.id.emailField);
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
+        mRegisterButton = (Button) findViewById(R.id.registerButton);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+        mGoogleLoginButton = (Button) findViewById(R.id.googleLoginButton);
+    }
+}
