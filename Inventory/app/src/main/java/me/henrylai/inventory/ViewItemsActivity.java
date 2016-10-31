674cc1db167cac4c002d2120ef9494ccbbe19151
Henry Lai <bzlai@aol.com>
2016-10-30 21:56:53 -0700

    Added some basic adapter functionality to enable access to item details activity

--- Inventory/app/src/main/java/me/henrylai/inventory/ViewItemsActivity.java
+++ Inventory/app/src/main/java/me/henrylai/inventory/ViewItemsActivity.java
@@ -1,5 +1,6 @@
 package me.henrylai.inventory;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,8 +8,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ViewItemsActivity extends AppCompatActivity {
+
+    private ListView mItemListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,28 @@ public class ViewItemsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_items);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // array of string of items
+        String[] items = {"Oranges", "Batteries", "porn"};
+
+        //setting up adapter for listvirw
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        // attach listview
+        mItemListView = (ListView) findViewById(R.id.items_listview);
+        mItemListView.setAdapter(itemsAdapter);
+        mItemListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ViewItemsActivity.this, ItemDetailsActivity.class);
+                //based on item add info to intent
+                startActivity(intent);
+            }
+
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
