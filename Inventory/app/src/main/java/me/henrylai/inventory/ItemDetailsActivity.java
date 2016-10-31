ae3d36b671314876813ebc1b4a0c7f97a7ceccbb
Can Huynh <canchuynh@aim.com>
2016-10-30 23:30:24 -0700

    Implement delete item comfirmation dialog.

--- Inventory/app/src/main/java/me/henrylai/inventory/ItemDetailsActivity.java
+++ Inventory/app/src/main/java/me/henrylai/inventory/ItemDetailsActivity.java
@@ -1,7 +1,10 @@
 package me.henrylai.inventory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ItemDetailsActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class ItemDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
+    }
+
+    public void deleteItem(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO: continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
