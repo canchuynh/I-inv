803cce5c958eca899b6f4bdfb6854ff7bf7be9ed
Can Huynh <canchuynh@aim.com>
2016-10-30 22:05:35 -0700

    Made logout fragment.

---
+++ Inventory/app/src/main/java/me/henrylai/inventory/MenuFragment.java
@@ -0,0 +1,52 @@
+package me.henrylai.inventory;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MenuFragment extends Fragment {
+
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout:
+                // TODO: logout here
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
