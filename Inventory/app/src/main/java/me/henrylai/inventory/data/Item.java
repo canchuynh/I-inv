39eb59ef7dd590621eac4d878229d27ef9639c8d
Henry Lai <bzlai@aol.com>
2016-11-03 12:01:55 -0700

    Added database methods for adding an item and getting items as well as additional fields (to be implemented later)

--- Inventory/app/src/main/java/me/henrylai/inventory/data/Item.java
+++ Inventory/app/src/main/java/me/henrylai/inventory/data/Item.java
@@ -6,6 +6,8 @@ package me.henrylai.inventory.data;
 
 public class Item {
 
+    /** The id of the item. */
+    private int mId;
     /** The name of the item. */
     private String mName;
     /** The value of the item. */
@@ -14,14 +16,38 @@ public class Item {
     private String mCondition;
     /** The description of the item. */
     private String mDescription;
+    private String mSerialNumber;
+    private String mMake;
+    private String mModel;
+
+    public Item() {
+
+    }
 
     /**
      * Constructs an item with the given name.
      * @param theName the name of the item.
      */
-    public Item (String theName) {
+    public Item (int theId, String theName) {
+        this.mId = theId;
         this.mName = theName;
     }
+
+    public Item (int theId, String theName, double theValue) {
+        this(theId, theName);
+        this.mValue = theValue;
+    }
+
+    public Item (int theId, String theName, double theValue, String theCondition) {
+        this(theId, theName, theValue);
+        this.mCondition = theCondition;
+    }
+
+    public Item (int theId, String theName, double theValue, String theCondition, String theDescription) {
+        this(theId, theName, theValue, theCondition);
+        this.mDescription = theDescription;
+    }
+
 
     public String getmName() {
         return mName;
@@ -53,5 +79,37 @@ public class Item {
 
     public void setmDescription(String mDescription) {
         this.mDescription = mDescription;
+    }
+
+    public String getmModel() {
+        return mModel;
+    }
+
+    public void setmModel(String mModel) {
+        this.mModel = mModel;
+    }
+
+    public String getmSerialNumber() {
+        return mSerialNumber;
+    }
+
+    public void setmSerialNumber(String mSerialNumber) {
+        this.mSerialNumber = mSerialNumber;
+    }
+
+    public String getmMake() {
+        return mMake;
+    }
+
+    public void setmMake(String mMake) {
+        this.mMake = mMake;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
     }
 }
--- Inventory/app/src/main/java/me/henrylai/inventory/data/SQLiteDBHandler.java
+++ Inventory/app/src/main/java/me/henrylai/inventory/data/SQLiteDBHandler.java
@@ -4,9 +4,14 @@ package me.henrylai.inventory.data;
  * Created by Starwater on 11/3/2016.
  */
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SQLiteDBHandler extends SQLiteOpenHelper {
 
@@ -31,7 +36,7 @@ public class SQLiteDBHandler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String CREATE_TABLE = "CREATE TABLE" + TABLE_ITEMS + "("
                 + KEY_ID + " INTEGER PRIMARY KEY,"
-                + NAME + " TEXT,"
+                + NAME + " TEXT NOT NULL,"
                 + VALUE + "REAL"
                 + CONDITION + "TEXT"
                 + DESCRIPTION + "TEXT"
@@ -45,4 +50,61 @@ public class SQLiteDBHandler extends SQLiteOpenHelper {
         // Creating tables again
         onCreate(db);
     }
+
+    // Adding new item
+    public void addItem(Item item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(NAME, item.getmName()); // Item Name
+        values.put(VALUE, item.getmValue()); // Item Value
+        values.put(CONDITION, item.getmCondition()); // Item Value
+        values.put(DESCRIPTION, item.getmDescription()); // Item Value
+
+        // Inserting Row
+        db.insert(TABLE_ITEMS, null, values);
+        db.close(); // Closing database connection
+    }
+
+    // Getting one item
+    public Item getItem(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_ITEMS, new String[]{KEY_ID,
+                NAME, VALUE, CONDITION, DESCRIPTION}, KEY_ID + "=?",
+        new String[]{String.valueOf(id)}, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Item item = new Item(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), Double.parseDouble(cursor.getString(2)), cursor.getString(3), cursor.getString(4));
+        // return item
+        return item;
+    }
+
+    // Getting All Items from DB
+    public List<Item> getAllItems() {
+        List<Item> itemList = new ArrayList<Item>();
+        // Select All Query
+        String selectQuery = "SELECT * FROM " + TABLE_ITEMS;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Item item = new Item();
+                item.setmId(Integer.parseInt(cursor.getString(0)));
+                item.setmName(cursor.getString(1));
+                item.setmValue(Double.parseDouble(cursor.getString(2)));
+                item.setmCondition(cursor.getString(3));
+                item.setmDescription(cursor.getString(4));
+                // Adding item to list
+                itemList.add(item);
+            } while (cursor.moveToNext());
+        }
+        // return item list
+        return itemList;
+    }
+
+
 }
