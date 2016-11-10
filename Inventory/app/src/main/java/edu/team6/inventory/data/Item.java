6602809b2207ea2085032fc1bc63ce026eb69724
Henry Lai <bzlai@aol.com>
2016-11-09 19:02:15 -0800

    Added the stuff for getters/setters >_<

--- Inventory/app/src/main/java/edu/team6/inventory/data/Item.java
+++ Inventory/app/src/main/java/edu/team6/inventory/data/Item.java
@@ -63,67 +63,79 @@ public class Item implements Serializable {
         this(0, theName, theValue, theCondition, theDescription);
     }
 
+    /** Gets the item name. */
     public String getmName() {
         return mName;
     }
 
+    /** Sets the item name. */
     public void setmName(String mName) {
         this.mName = mName;
     }
 
+    /** Gets the item value. */
     public double getmValue() {
         return mValue;
     }
 
+    /** Sets the item value. */
     public void setmValue(double mValue) {
         this.mValue = mValue;
     }
 
+    /** Gets the item condition. */
     public String getmCondition() {
         return mCondition;
     }
 
+    /** Sets the item condition. */
     public void setmCondition(String mCondition) {
         this.mCondition = mCondition;
     }
 
+    /** Gets the item description. */
     public String getmDescription() {
         return mDescription;
     }
 
+    /** Sets the item description. */
     public void setmDescription(String mDescription) {
         this.mDescription = mDescription;
     }
 
+    /** Gets the item model. */
     public String getmModel() {
         return mModel;
     }
 
+    /** Sets the item model. */
     public void setmModel(String mModel) {
         this.mModel = mModel;
     }
 
+    /** Gets the item serial number. */
     public String getmSerialNumber() {
         return mSerialNumber;
     }
 
+    /** Sets the item serial number. */
     public void setmSerialNumber(String mSerialNumber) {
         this.mSerialNumber = mSerialNumber;
     }
 
+    /** Gets the item make. */
     public String getmMake() {
         return mMake;
     }
 
+    /** Sets the item make. */
     public void setmMake(String mMake) {
         this.mMake = mMake;
     }
 
+    /** Gets the item id. */
     public int getmId() {
         return mId;
     }
 
-    public void setmId(int mId) {
-        this.mId = mId;
-    }
 }
