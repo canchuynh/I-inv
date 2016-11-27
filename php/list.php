7150b2e0a8ab7fc5b39d0df031cdca29bea04259
Can Huynh <canchuynh@aim.com>
2016-11-27 03:50:34 -0800

    Change list.php to query user's table.

--- php/list.php
+++ php/list.php
@@ -1,7 +1,6 @@
 <?PHP
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
-$command = $_GET['cmd'];
 
 	// Connect to the Database
 	$dsn = 'mysql:host=cssgate.insttech.washington.edu;dbname=canhuynh';
@@ -10,13 +9,15 @@ $command = $_GET['cmd'];
 
     	try {
         	$db = new PDO($dsn, $username, $password);
-	        	
-			if ($command == "inventory") {
-				$select_sql = 'SELECT * FROM Inventory';
+			
+			$userId = isset($_GET['userId']) ? $_GET['userId'] : '';
+	        
+			if ($userId != '') {
+				$select_sql = 'SELECT * FROM ' . $username . '.' . $userId;
 				$item_query = $db->query($select_sql);
 				$items = $item_query->fetchAll(PDO::FETCH_ASSOC);
 				if ($items) {	
-	   				echo json_encode($items);
+					echo json_encode($items);
 				}
 			}
     	} catch (PDOException $e) {
