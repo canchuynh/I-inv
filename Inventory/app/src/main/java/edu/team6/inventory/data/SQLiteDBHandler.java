8ed4c1eda4695b51d1ebb495d50aa87f8dfe8066
Henry Lai <bzlai@aol.com>
2016-12-05 18:50:16 -0800

    Fixed bug regarding getCount for SQLiteDB

--- Inventory/app/src/main/java/edu/team6/inventory/data/SQLiteDBHandler.java
+++ Inventory/app/src/main/java/edu/team6/inventory/data/SQLiteDBHandler.java
@@ -109,9 +109,10 @@ public class SQLiteDBHandler extends SQLiteOpenHelper {
         String countQuery = "SELECT * FROM " + TABLE_ITEMS;
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(countQuery, null);
+        int count = cursor.getCount();
         cursor.close();
         // return count
-        return cursor.getCount();
+        return count;
     }
 
     /**
